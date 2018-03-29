90dcff524a89cc5d6dd413d2090f21376cef7ddd
Needy <christian139601@gmail.com>
2018-03-29 15:25:45 +0100

    stage 1

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,13 +14,13 @@ module.exports = function(grunt) {
         options: {
           engine: 'im',
           sizes: [{
-            /*
-            Change these:
-            
-            width: ,
-            suffix: ,
-            quality:
-            */
+            width: '30%',
+            suffix: '_small',
+            quality: 20
+          },{
+            width: '50%',
+            suffix: '_large',
+            quality: 40
           }]
         },
 
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
       },
     },
   });
-  
+
   grunt.loadNpmTasks('grunt-responsive-images');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
